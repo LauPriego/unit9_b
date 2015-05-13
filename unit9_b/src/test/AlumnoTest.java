e947f277b5d3793e75a285d03ac6a190ed6ca425
Lau <lau.priego@gmail.com>
2015-05-13 21:59:33 +0200

    CLASE TEST CREADA PERO NO IMPLEMENTADA

---
+++ unit9_b/src/test/AlumnoTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Laura Martinez Priego 1º DAM
+ *
+ */
+public class AlumnoTest {
+
+	@Test
+	public void testValidaNif() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testCalculaTasaMatricula() {
+		fail("No implementado aun");
+	}
+
+}
