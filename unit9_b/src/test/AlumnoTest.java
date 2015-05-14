10de173c1d3f1a0c45558e26ac8efaaf94747bd8
Lau <lau.priego@gmail.com>
2015-05-14 16:48:11 +0200

    TEST METODO TASAMATRÍCULA IMPLEMENTADOS Y FUNCIONALES

--- unit9_b/src/test/AlumnoTest.java
+++ unit9_b/src/test/AlumnoTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import unit9_b.Alumno;
+
 /**
  * 
  * @author Laura Martinez Priego 1º DAM
@@ -12,13 +14,109 @@ import org.junit.Test;
 public class AlumnoTest {
 
 	@Test
-	public void testValidaNif() {
-		fail("No implementado aun");
+	public void testValidaNif1() 
+	{
+		Alumno a = new Alumno(); 
+		String nif = "123456";
+		
+		boolean resultadoEsperado = false;
+		boolean resultadoObtenido = a.validaNif(nif);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	@Test
+	public void testValidaNif2() 
+	{
+		Alumno a = new Alumno(); 
+		String nif = "29194741J";
+		
+		boolean resultadoEsperado = true;
+		boolean resultadoObtenido = a.validaNif(nif);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	@Test
+	public void testValidaNif3() 
+	{
+		Alumno a = new Alumno(); 
+		String nif = "7HHH444K";
+		
+		boolean resultadoEsperado = false;
+		boolean resultadoObtenido = a.validaNif(nif);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	@Test
+	public void testValidaNif4() 
+	{
+		Alumno a = new Alumno(); 
+		String nif = "";
+		
+		boolean resultadoEsperado = false;
+		boolean resultadoObtenido = a.validaNif(nif);
+		assertEquals(resultadoEsperado, resultadoObtenido);
 	}
 
+	
+	
+	
+	
 	@Test
-	public void testCalculaTasaMatricula() {
-		fail("No implementado aun");
+	public void testCalculaTasaMatricula1() 
+	{
+		Alumno a = new Alumno(); 
+		int edad = 30;
+		boolean familiaNumerosa = true;
+		boolean repetidor = true;
+		
+		float resultadoEsperado = 250;
+		float resultadoObtenido = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		assertTrue(resultadoEsperado == resultadoObtenido);
+	}
+	@Test
+	public void testCalculaTasaMatricula2() 
+	{
+		Alumno a = new Alumno(); 
+		int edad = 67;
+		boolean familiaNumerosa = false;
+		boolean repetidor = false;
+		
+		float resultadoEsperado = 250;
+		float resultadoObtenido = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		assertTrue(resultadoEsperado == resultadoObtenido);
+	}
+	@Test
+	public void testCalculaTasaMatricula3() 
+	{
+		Alumno a = new Alumno(); 
+		int edad = 23;
+		boolean familiaNumerosa = false;
+		boolean repetidor = true;
+		
+		float resultadoEsperado = 2000;
+		float resultadoObtenido = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		assertTrue(resultadoEsperado == resultadoObtenido);
+	}
+	@Test
+	public void testCalculaTasaMatricula4() 
+	{
+		Alumno a = new Alumno(); 
+		int edad = 23;
+		boolean familiaNumerosa = false;
+		boolean repetidor = false;
+		
+		float resultadoEsperado = 500;
+		float resultadoObtenido = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		assertTrue(resultadoEsperado == resultadoObtenido);
+	}
+	@Test
+	public void testCalculaTasaMatricula5() 
+	{
+		Alumno a = new Alumno(); 
+		int edad = 63;
+		boolean familiaNumerosa = false;
+		boolean repetidor = false;
+		
+		float resultadoEsperado = 400;
+		float resultadoObtenido = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		assertTrue(resultadoEsperado == resultadoObtenido);
 	}
 
 }
